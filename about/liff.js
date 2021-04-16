b0e36e35923be6f463ba24c05efd51de66cf25dd
holykzm <64673947+holykzm@users.noreply.github.com>
2021-04-16 14:05:29 +0900

    Add files via upload

---
+++ about/liff.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
+    // LINE DevelopersのLIFF画面より確認可能
+    var liffId = "1655660147-9aqxQyeP";
+    initializeLiff(liffId);
+})
+
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
+        })
+        .then(() => {
+            // Webブラウザからアクセスされた場合は、LINEにログインする
+            if (!liff.isInClient() && !liff.isLoggedIn()) {
+                window.alert("LINEアカウントにログインしてください。");
+                liff.login({redirectUri: location.href});
+            }
+        })
+        .catch((err) => {
+            console.log('LIFF Initialization failed ', err);
+        });
+}
+
+
+}
