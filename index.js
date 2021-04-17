fa050127a3d421efa433ae88b28d4f0b9a8f5cda
holykzm <64673947+holykzm@users.noreply.github.com>
2021-04-17 12:32:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ $(function () {
         var time32 = $('#timepicker2-2').val();
         
         var names = $('input[name="names"]').val();
+　　　　　var staff = $('input[name="s-name"]:checked').val();
 
         var menu = $('input[name="menu"]:checked').map(function(){
           return $(this).val();
@@ -88,7 +89,7 @@ $(function () {
         var memo = $('input[name="memo"]').val();
 
 
-        var msg = `希望日：${date}\n\n時間：${time}～${time2}までの間\n\n（第２希望）\n ${date2}\n${time21}～${time22}までの間\n\n（第３希望）\n${date3}\n${time31}～${time32}までの間\n\n氏名：${names}様\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
+        var msg = `希望日：${date}\n\n時間：${time}～${time2}までの間\n\n（第２希望）\n ${date2}\n${time21}～${time22}までの間\n\n（第３希望）\n${date3}\n${time31}～${time32}までの間\n\n氏名：${names}様\n\n 担当指名：${staff}\n\nメニュー：${menu}\n\n概要：${memo}\n\n\n送信ありがとうございます！\n返信が来るまでしばらくお待ちください♪`;
         sendText(msg);
 
         return false;
